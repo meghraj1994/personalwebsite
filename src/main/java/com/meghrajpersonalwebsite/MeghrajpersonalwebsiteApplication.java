4e08973fa431d31a541e23643048cadb4f14e35c
meghrajkurmi <kurmimeghraj@gmail.com>
2022-04-10 23:22:28 -0500

    "first commit"

---
+++ src/main/java/com/meghrajpersonalwebsite/MeghrajpersonalwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.meghrajpersonalwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeghrajpersonalwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeghrajpersonalwebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meghrajpersonalwebsite/MeghrajpersonalwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meghrajpersonalwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeghrajpersonalwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
